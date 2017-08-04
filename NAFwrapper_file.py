a531b2904f37f4724495761ad74e69bd0e5224d4
Marieke van Erp <marieke.van.erp@vu.nl>
2017-08-04 16:11:39 +0200

    no message

--- NAFwrapper_file.py
+++ NAFwrapper_file.py
@@ -18,7 +18,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-f', nargs=1, help='Provide an input file. Fileformat: naf with entity layer.', dest='file', required=True)
+parser.add_argument('-f', nargs=1, help='Provide an input file. Fileformat: naf with entity layer (compulsory).', dest='file', required=True)
 parser.add_argument('-l', nargs=1, help='Choose language: nl or es. Default is nl.', dest='lang', default=['nl'])
 parser.add_argument('-t', nargs=1, help='Choose type hierarchy: gft or dbp. Default is gft.', dest='type', default=['gft'])
 parser.add_argument('-n', nargs=1, help='Take the NER type into account as well: y or n. Default is n.', dest='nertype', default=['n'])
